483ffb8d45e67fa7a324294150bc9e5d02f12836
Devam M <devam493@gmail.com>
2019-06-03 13:17:51 -0400

    added functionality to add and display flag

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,26 @@ import React from 'react'
 class Square extends React.Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            open: false,
+            flag: false
+        }
+        this.onClick = this.onClick.bind(this);
+        this.onRightClick = this.onRightClick.bind(this);
     }    
+
+    onClick(e, coord){
+        this.setState({open: true});
+        this.props.handleClick(e, coord)
+    }
+
+    onRightClick(e, coord){
+        if(this.state.open == false){
+            this.setState({flag: true})
+        }
+        
+    }
     
     render(){
         let {elem, x,y, handleClick} = this.props;
@@ -12,13 +31,18 @@ class Square extends React.Component {
             elem = ""
         }
         return(
-            <div className="square" onClick={ (e) => handleClick(e, coord) }>
-                {elem == 'x' ? (<img src="bomb.png" />) : (<h2>{elem}</h2>)}
+            <div className={`square ${this.state.open == true ? 'open' : ''}`} onClick={ (e) => this.onClick(e, coord) }
+            onContextMenu={(e) => {this.onRightClick(e, coord)}} >
+                {this.state.flag == false ?
+                    elem == 'x' ? (<img src="bomb.png" />) : (<h1>{elem}</h1>) :
+                    (<img src="flag.png" />)}
                 
             </div>
             
         )
     }
 }
+
+
 
 export default Square;
